7938b81939eaf6537161e3b9fca8f035ad095a24
Vikram5784 <141595239+Vikram5784@users.noreply.github.com>
2023-08-07 12:51:22 -0500

    Add files via upload

---
+++ emi.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def calculate_emi(p, n, r):
+  emi = p * (r/100) * ((1+(r/100))**n)/((1+(r/100)**n)-1)
+  st.write("emi calculated = ", emi)
+st.title("EMI calculator")
+p=st.slider("loan amount", 100000,10000000)
+n=st.slider("tenure", 24,36)
+r=st.slider("interest", 12,16)
+if st.button("calculate"):
+  calculate_emi(p)
